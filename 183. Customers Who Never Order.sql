d1f0e0aebd5ab94aca00e53302db1281b0244d01
Abdelrahman Helmy <104210345+Helmy-JR@users.noreply.github.com>
2025-08-20 21:00:31 +0300

    Update and rename 183.Customers Who Never Order.sql to 183. Customers Who Never Order.sql

--- 183. Customers Who Never Order.sql
+++ 183. Customers Who Never Order.sql
@@ -1,9 +1,12 @@
 # Write your MySQL query statement below
+
 SELECT name AS Customers
 FROM Customers
 WHERE id NOT IN (select customerId FROM Orders);
 
 -----------------------------------------------------
+# Write your MySQL query statement below
+    
 SELECT name AS Customers
 FROM Customers c
 LEFT JOIN Orders o
